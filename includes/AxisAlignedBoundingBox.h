127950929ec6e265204a03b8029c338e3134eb4a
J0sa3210 <joeri.winckelmans@outlook.be>
2023-11-22 14:25:56 +0100

    Added basic methods

--- includes/AxisAlignedBoundingBox.h
+++ includes/AxisAlignedBoundingBox.h
@@ -5,4 +5,48 @@
 #ifndef QUADTREE_AXISALIGNEDBOUNDINGBOX_H
 #define QUADTREE_AXISALIGNEDBOUNDINGBOX_H
 
+#include <tuple>
+#include <set>
+#include <ostream>
+
+using point = std::tuple<double,double>;
+
+
+// This class represents an axis-aligned bounding box
+// In a 2D problem, this is simply a rectangle whose sides are
+// parallel to the X- and Y-axis
+class AxisAlignedBoundingBox {
+private:
+    point origin;                           // The upper-left corner of the rectangle
+    double length;                          // The length of the rectangle (x-axis)
+    double height;                          // The height of the rectangle (y-axis)
+
+public:
+    // Constructor
+    AxisAlignedBoundingBox(point origin, double length, double height);
+
+    // Getters and setters
+    const point &getOrigin() const;
+
+    void setOrigin(const point &newOrigin);
+
+    double getLength() const;
+
+    void setLength(double newLength);
+
+    double getHeight() const;
+
+    void setHeight(double newHeight);
+
+
+    friend std::ostream &operator<<(std::ostream &os, const AxisAlignedBoundingBox &box);
+
+    // This friend function (A free function that can access private fields) should check if two AABBs overlap
+    // It returns true if there is overlap, false if there isn’t
+    friend bool collides(
+            const AxisAlignedBoundingBox &one,
+            const AxisAlignedBoundingBox &two);
+};
+
+
 #endif //QUADTREE_AXISALIGNEDBOUNDINGBOX_H
--- src/AxisAlignedBoundingBox.cpp
+++ src/AxisAlignedBoundingBox.cpp
@@ -4,4 +4,41 @@
 
 #include "AxisAlignedBoundingBox.h"
 
+#include <utility>
 
+
+AxisAlignedBoundingBox::AxisAlignedBoundingBox(point origin, double length, double height)
+        : origin(std::move(origin)), length(length), height(height) {}
+
+const point &AxisAlignedBoundingBox::getOrigin() const {
+    return origin;
+}
+
+void AxisAlignedBoundingBox::setOrigin(const point &newOrigin) {
+    AxisAlignedBoundingBox::origin = newOrigin;
+}
+
+double AxisAlignedBoundingBox::getLength() const {
+    return length;
+}
+
+void AxisAlignedBoundingBox::setLength(double newLength) {
+    AxisAlignedBoundingBox::length = newLength;
+}
+
+double AxisAlignedBoundingBox::getHeight() const {
+    return height;
+}
+
+void AxisAlignedBoundingBox::setHeight(double newHeight) {
+    AxisAlignedBoundingBox::height = newHeight;
+}
+
+std::ostream &operator<<(std::ostream &os, const AxisAlignedBoundingBox &box) {
+    os << "origin: (" << get<0>(box.origin) << ", " << get<1>(box.origin) << "), length: " << box.length << ", height: " << box.height;
+    return os;
+}
+
+bool collides(const AxisAlignedBoundingBox &one, const AxisAlignedBoundingBox &two) {
+    return false;
+}
