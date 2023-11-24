8f0077ce5e72aea5f5fe160e4985f5a1bc5061e9
J0sa3210 <joeri.winckelmans@outlook.be>
2023-11-24 12:50:10 +0100

    Replaced all functioncs from .cpp to .h
    This was necessary because of the way templates work in C++

--- includes/Quadtree/QuadTree.h
+++ includes/Quadtree/QuadTree.h
@@ -7,7 +7,9 @@
 
 
 #include <vector>
+#include <ostream>
 #include "AxisAlignedBoundingBox.h"
+
 
 // This class is our actual quadtree
 // It stores AABB’s together with some metadata
@@ -16,9 +18,9 @@ template<typename MetadataType>
 class Quadtree {
 private:
     AxisAlignedBoundingBox bounds;
-    unsigned int capacity;
+    unsigned int region_capacity;
 
-    std::vector<std::tuple<AxisAlignedBoundingBox,MetadataType>> objects;
+    std::vector<std::tuple<AxisAlignedBoundingBox, MetadataType>> objects;
 
     bool subdivided = false;
 
@@ -27,40 +29,69 @@ private:
     Quadtree<MetadataType> *southWest = nullptr;
     Quadtree<MetadataType> *northWest = nullptr;
 
+    void divide(){
+        Point origin = bounds.getOrigin();
+        double length = bounds.getLength();
+        double height = bounds.getHeight();
+
+        AxisAlignedBoundingBox ne = AxisAlignedBoundingBox(origin + Point(length / 2, -height / 2), length / 2, height / 2);
+        this->northEast = new Quadtree<MetadataType>(ne, this->region_capacity);
+        AxisAlignedBoundingBox se = AxisAlignedBoundingBox(origin + Point(length / 2, height / 2), length / 2, height / 2);
+        this->northEast = new Quadtree<MetadataType>(se, this->region_capacity);
+        AxisAlignedBoundingBox sw = AxisAlignedBoundingBox(origin + Point(-length / 2, height / 2), length / 2, height / 2);
+        this->northEast = new Quadtree<MetadataType>(sw, this->region_capacity);
+        AxisAlignedBoundingBox nw = AxisAlignedBoundingBox(origin + Point(-length / 2, -height / 2), length / 2,
+                                                           height / 2);
+        this->northEast = new Quadtree<MetadataType>(nw, this->region_capacity);
+
+        this->subdivided = true;
+    };
+
+    void redivide();
+
 public:
     // Constructor
     // `bounds` specifies the edges of the region that the quadtree covers.
     // `region_capacity` specifies the maximum number of objects in a single region.
-    Quadtree(const AxisAlignedBoundingBox &bounds, unsigned int region_capacity);
+    Quadtree<MetadataType>(const AxisAlignedBoundingBox &bounds, unsigned int region_capacity) :
+            bounds(bounds),
+            region_capacity(region_capacity) {};
 
     // This method inserts the given metadata and
     // AABB into the quadtree.
-    void insert(const AxisAlignedBoundingBox &aabb, const MetadataType &meta);
+    void insert(const AxisAlignedBoundingBox &aabb, const MetadataType &meta) {
+        if (!subdivided) {
+            if (this->objects.size() < this->region_capacity) {
+                this->objects.push_back({aabb, meta});
+            } else {
+                divide();
+                //redivide();
+            }
+        } else {
+            if (this->northEast->bounds.contains(aabb)) {
+                northEast->insert(aabb, meta);
+            } else if (this->southEast->bounds.contains(aabb)) {
+                southEast->insert(aabb, meta);
+            } else if (this->southWest->bounds.contains(aabb)) {
+                southWest->insert(aabb, meta);
+            } else if (this->northWest->bounds.contains(aabb)) {
+                northWest->insert(aabb, meta);
+            } else {
+                this->objects.push_back({aabb, meta});
+            }
+        }
+    };
 
-// This method should return a std::unordered_set of
-// all items inside the given AABB.
-// The given AABB may span multiple square regions of the quadtree.
-// The worst-case time-complexity of this method should be O(log(N)) for a Quadtree with N leaf nodes
-// TODO: You should decide the element type of the std::unordered_set
-// Your set should contain the AABB and the Metadata of all objects in the given region.
-    //std::unordered_set<auto> query_region(const AxisAlignedBoundingBox& aabb) const;
-// TODO: You should decide the return type for this method
-// This method should return an iterator that runs over
-// every element in the quadtree.
-    auto begin();
+    bool isSubdivided() const {
+        return subdivided;
+    };
 
-// TODO: You should decide the return type for this method
-// This method should return an iterator pointing
-// one past the last element in the quadtree.
-// When iterating from begin() to end(),
-// we should visit all elements in the quadtree.
-// There is no requirement with regards to
-// the order that you visit these elements in.
-    auto end();
-
-    void divide();
-
-    void redivide();
+    friend std::ostream &operator<<(std::ostream &os, const Quadtree &quadtree) {
+        os << "bounds: " << quadtree.bounds << " region_capacity: " << quadtree.region_capacity << " subdivided: " << quadtree.subdivided << " northEast: " << quadtree.northEast
+           << " southEast: " << quadtree.southEast << " southWest: " << quadtree.southWest << " northWest: "
+           << quadtree.northWest;
+        return os;
+    }
 };
 
 
--- test/QuadTreeTest.cpp
+++ test/QuadTreeTest.cpp
@@ -1,3 +1,16 @@
 //
 // Created by Joeri on 11/23/2023.
 //
+
+#include <iostream>
+#include "QuadTree.h"
+#include "catch.hpp"
+
+TEST_CASE("Test qt"){
+    AxisAlignedBoundingBox object1 = AxisAlignedBoundingBox({10,10},5,5);
+    AxisAlignedBoundingBox object2 = AxisAlignedBoundingBox({12,12},1,1);
+    Quadtree<int> qt = Quadtree<int>(object1,2);
+    qt.insert(object2,3);
+    REQUIRE(qt.isSubdivided() == false);
+    std::cout << qt << std::endl;
+}
